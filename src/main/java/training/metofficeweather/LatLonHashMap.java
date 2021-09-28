b0bf225889e06690ded2c559b6ed6f1ae55c3bf5
Gregory Barratt <barrattg@hotmail.co.uk>
2021-09-28 14:10:48 +0100

    adding fake locations

--- src/main/java/training/metofficeweather/LatLonHashMap.java
+++ src/main/java/training/metofficeweather/LatLonHashMap.java
@@ -1,10 +1,14 @@
 package training.metofficeweather;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
-public class LatLonHashMap {
+//public class LatLonHashMap {
+    //public static void main(String[] args)
+    //{
 
-    Map<String, Map<String, String>> LatLonHashMap = new HashMap<String, Map<String, String>>();
-    //map of location and lat/lon
-
-}
+    //Map<String, Map<String, String>> latLonHashMap = new HashMap<>();
+    //map of location and lat/lon((
+   //latLonHashMap.put("Cambrige", "1", ""2");
+//}
+//}
--- src/main/java/training/metofficeweather/WeatherController.java
+++ src/main/java/training/metofficeweather/WeatherController.java
@@ -4,22 +4,39 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.http.MediaType;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class WeatherController {
 
 
-    String [] fakeLocations = {"Cambridge", "London", "Bath"};
+
     @RequestMapping("/weather") // page for user to enter place ID
-    ModelAndView home() {
-        return new ModelAndView("index", "locations", fakeLocations);
+    String home(Model myModel) {
+        String [] fakeLocations = {"Cambridge", "London", "Bath"};
+        myModel.addAttribute("locations", fakeLocations);
+        return "hello";
+    }
+
+    @RequestMapping(
+            value = "/weatherForLocation",
+            method= RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE
+    )
+    ModelAndView weatherForLocation(@RequestParam Map<String, String> body) {
+        System.out.println("just:" + body);
+        return new ModelAndView("info", "location", body) ;
     }
 
     @RequestMapping("/weatherInfo") // page for putting result data from place ID
@@ -29,7 +46,7 @@ public class WeatherController {
         try {
             Client weatherClient = ClientBuilder.newClient();
             String weatherApp = weatherClient.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + locationId + "?res=3hourly&key=52452172-a037-42d6-8d10-07c179cccb42")
-                    .request(MediaType.TEXT_PLAIN)
+                    .request(javax.ws.rs.core.MediaType.TEXT_PLAIN)
                     .get(String.class);
             WeatherRoot weatherRoot = objectMapper.readValue(weatherApp, WeatherRoot.class);
 
